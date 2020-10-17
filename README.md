910d598c1c7e6ba11e3130f6de9f57a382eed1b3
hezron97 <42761558+hezron97@users.noreply.github.com>
2020-10-17 22:12:52 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# covid19
-latihan
+# COVID
+# HEZRON HEZEKIAH LESMANA
+# 20185520008
+# PEMOGRAMAN MOBILE
+A new Flutter project.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
+
---
+++ main.dart
@@ -0,0 +1,238 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    title: "COVID 19",
+    home: new Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => new _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<Container> covid = new List();
+
+  var karakter = [
+    {"nama": "Beranda", "gambar": "beranda.jpg"},
+    {"nama": "Berita", "gambar": "berita.jpg"},
+    {"nama": "Data Covid ", "gambar": "data.jpg"},
+    {"nama": "Peraturan", "gambar": "peraturan.jpg"},
+    {"nama": "Edukasi", "gambar": "edukasi.jpg"},
+    {"nama": "Tanya Jawab", "gambar": "tanya.jpg"},
+    {"nama": "Agenda", "gambar": "agenda.jpg"},
+    {"nama": "Info Lain", "gambar": "info.jpg"},
+    {"nama": "Hoax Buster", "gambar": "hoax.jpg"},
+    {"nama": "Mencari", "gambar": "mencari.jpg"},
+  ];
+
+  _buatlist() async {
+    for (var i = 0; i < karakter.length; i++) {
+      final karakternya = karakter[i];
+      final String gambar = karakternya["gambar"];
+
+      covid.add(new Container(
+          padding: new EdgeInsets.all(10.0),
+          child: new Card(
+              child: new Column(
+            children: <Widget>[
+              new Hero(
+                tag: karakternya['nama'],
+                child: new Material(
+                  child: new InkWell(
+                    onTap: () =>
+                        Navigator.of(context).push(new MaterialPageRoute(
+                              builder: (BuildContext context) => new Detail(
+                                    nama: karakternya['nama'],
+                                    gambar: gambar,
+                                  ),
+                            )),
+                    child: new Image.asset(
+                      "img/$gambar",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              new Padding(
+                padding: new EdgeInsets.all(10.0),
+              ),
+              new Text(
+                karakternya['nama'],
+                style: new TextStyle(fontSize: 20.0),
+              )
+            ],
+          ))));
+    }
+  }
+
+  @override
+  void initState() {
+    _buatlist();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(
+          "Covid 19",
+          style: new TextStyle(color: Colors.white),
+        ),
+      ),
+      body: new GridView.count(
+        crossAxisCount: 2,
+        children: covid,
+      ),
+    );
+  }
+}
+
+class Detail extends StatelessWidget {
+  Detail({this.nama, this.gambar});
+  final String nama;
+  final String gambar;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new ListView(
+        children: <Widget>[
+          new Container(
+              height: 240.0,
+              child: new Hero(
+                tag: nama,
+                child: new Material(
+                  child: new InkWell(
+                    child: new Image.asset(
+                      "img/$gambar",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              )),
+          new BagianNama(
+            nama: nama,
+          ),
+          new BagianIcon(),
+          new Keterangan(),
+        ],
+      ),
+    );
+  }
+}
+
+class BagianNama extends StatelessWidget {
+  BagianNama({this.nama});
+  final String nama;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      padding: new EdgeInsets.all(10.0),
+      child: new Row(
+        children: <Widget>[
+          new Expanded(
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Text(
+                  nama,
+                  style: new TextStyle(fontSize: 20.0, color: Colors.black),
+                ),
+                new Text(
+                  "$nama\@mu.COM",
+                  style: new TextStyle(fontSize: 17.0, color: Colors.grey),
+                ),
+              ],
+            ),
+          ),
+          new Row(
+            children: <Widget>[
+              new Icon(
+                Icons.star,
+                size: 30.0,
+                color: Colors.red,
+              ),
+              new Text(
+                "Matana University",
+                style: new TextStyle(fontSize: 18.0),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class BagianIcon extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      padding: new EdgeInsets.all(10.0),
+      child: new Row(
+        children: <Widget>[
+          new Iconteks(
+            icon: Icons.call,
+            teks: "Call",
+          ),
+          new Iconteks(
+            icon: Icons.message,
+            teks: "Message",
+          ),
+          new Iconteks(
+            icon: Icons.photo,
+            teks: "Photo",
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Iconteks extends StatelessWidget {
+  Iconteks({this.icon, this.teks});
+  final IconData icon;
+  final String teks;
+  @override
+  Widget build(BuildContext context) {
+    return new Expanded(
+      child: new Column(
+        children: <Widget>[
+          new Icon(
+            icon,
+            size: 50.0,
+            color: Colors.black,
+          ),
+          new Text(
+            teks,
+            style: new TextStyle(fontSize: 18.0, color: Colors.black),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Keterangan extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      padding: new EdgeInsets.all(10.0),
+      child: new Card(
+        child: new Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: new Text(
+            "COE-5109 Pemograman Mobile Dosen : Ary Budi Warsito ,S.kom,M.kom Hezron Hezekiah Lesmana 20185520008",
+            style: new TextStyle(fontSize: 18.0),
+            textAlign: TextAlign.justify,
+          ),
+        ),
+      ),
+    );
+  }
+}
